aa99e4a203c43e499c3ecf94966a317b0c6d9aa6
Tugay Demirel <tugaydemirelyedek@gmail.com>
2021-03-05 15:59:36 +0300

    first commit

---
+++ src/components/MemoryCard.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+import './MemoryCard.scss';
+
+class Card extends Component {
+  render() {
+    let innerClass = "MemoryCard__inner";
+    if (this.props.isFlipped) {
+      innerClass += ' flipped';
+    }
+    return (
+      <div className="MemoryCard" onClick={this.props.pickCard}>
+        <div className={innerClass}>
+          <div className="MemoryCard__back">
+            ♫
+          </div>
+          <div className="MemoryCard__front">
+            {this.props.musician}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Card;
